44c24f783ee1cf8c29b49b206eeab441b737cdfe
Omkar Dige <oyedige@gmail.com>
2023-12-20 07:57:37 -0500

    Refactored code by abstracting components to classes

--- pubmed_qa/main.py
+++ pubmed_qa/main.py
@@ -1,195 +1,113 @@
-import os
-import argparse
 import random
-import chromadb
-import re
 
-from pathlib import Path
-import llama_index
-from llama_index import (
-    VectorStoreIndex, ServiceContext, PromptTemplate,
-    set_global_service_context, load_index_from_storage, get_response_synthesizer,
-)
-from llama_index.embeddings import HuggingFaceEmbedding, OpenAIEmbedding
-from llama_index.llms import HuggingFaceLLM, OpenAI
+from llama_index import ServiceContext, set_global_service_context, set_global_handler
 from llama_index.text_splitter import SentenceSplitter
-from llama_index.vector_stores import ChromaVectorStore
-from llama_index.storage.storage_context import StorageContext
-from llama_index.retrievers import VectorIndexRetriever
-from llama_index.query_engine import RetrieverQueryEngine
-from llama_index.postprocessor import SimilarityPostprocessor, LLMRerank, SentenceEmbeddingOptimizer
-from llama_index.schema import MetadataMode
 
 from task_dataset import PubMedQATaskDataset
-from rag_utils import DocumentReader, extract_yes_no, evaluate
+from rag_utils import (
+    DocumentReader, RAGEmbedding, RAGLLM, RAGIndex, RAGQueryEngine, 
+    extract_yes_no, evaluate
+    )
 
 
 def main():
 
-    # Set RAG configuration, TODO - Add elements while developing
-    embed_model_type = 'hf' # Type of embedding model to choose from
-    llm_type = 'local' # Whether to load local LLM or use Open AI API 
-    
+    # Set RAG configuration, TODO - Create json file
+    rag_cfg = {
+        # Node parser config
+        "chunk_size": 256,
+        "chunk_overlap": 0,
+
+        # Embedding model config
+        "embed_model_type": "hf",
+        "embed_model_name": "BAAI/bge-base-en-v1.5",
+
+        # LLM config
+        "llm_type": "local",
+        "llm_name": "Llama-2-7b-chat-hf",
+        "max_new_tokens": 256,
+        "temperature": 1.0,
+        "top_p": 1.0,
+        "top_k": 50,
+        "do_sample": False,
+
+        # Vector DB config
+        "vector_db_type": "chromadb",
+        "vector_db_name": "pubmed_qa",
+
+        # Retriever and query config
+        "retriever_type": "vector_index",
+        "retriever_similarity_top_k": 3,
+        "response_mode": "compact",
+    }
+
+    # Set handler for debugging
+    # https://docs.llamaindex.ai/en/stable/module_guides/observability/observability.html
+    set_global_handler("simple")
+
+
+    ### STAGE 1 - Load data and documents
+    # 1. Load QA data
     print('Loading PubMed QA data ...')
     pubmed_data = PubMedQATaskDataset('bigbio/pubmed_qa')
     print(len(pubmed_data))
     # print(pubmed_data[9])
     # pubmed_data.mock_knowledge_base(output_dir='./data')
 
-    # # Set handler for debugging
-    # # https://docs.llamaindex.ai/en/stable/module_guides/observability/observability.html
-    # llama_index.set_global_handler("simple")
-    
-
-    ### LOADING STAGE
-    # 1. Load data
+    # 2. Load documents
     print('Loading documents ...')
     reader = DocumentReader(input_dir="./data/pubmed_doc")
     docs = reader.load_data()
     print(f'No. of documents loaded: {len(docs)}')
 
-    # 2. Split documents into smaller chunks
+
+    ### STAGE 2 - Load node parser, embedding and LLM and set service context
+    # 1. Load node parser to split documents into smaller chunks
     print('Loading node parser ...')
-    chunk_size = 256
-    chunk_overlap = 0
-    node_parser = SentenceSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    node_parser = SentenceSplitter(chunk_size=rag_cfg['chunk_size'], chunk_overlap=rag_cfg['chunk_overlap'])
     # nodes = node_parser.get_nodes_from_documents(docs)
-    # print(len(nodes))
 
+    # 2. Load embedding model
+    embed_model = RAGEmbedding(model_type=rag_cfg['embed_model_type'], model_name=rag_cfg['embed_model_name']).load_model()
 
-    ### INDEXING STAGE
-    # 1. Load embedding model
-    # Llama-index supports embedding models from OpenAI, Cohere, LangChain, HuggingFace, etc. 
-    # We can also build out custom embedding model.
-    # https://docs.llamaindex.ai/en/stable/module_guides/models/embeddings.html
-    print(f'Loading {embed_model_type} embedding model ...')
-    if embed_model_type == 'hf':
-        # Using bge base HuggingFace embeddings, can choose others based on leaderboard: 
-        # https://huggingface.co/spaces/mteb/leaderboard
-        embed_model = HuggingFaceEmbedding(
-            model_name="BAAI/bge-base-en-v1.5") # max_length does not have any effect?
-    elif embed_model_type == 'openai':
-        # TODO - Add open ai embedding model
-        # embed_model = OpenAIEmbedding()
-        raise NotImplementedError
-    # sample_text = nodes[0].text
-    # sample_text_embedding = embed_model.get_text_embedding(sample_text)
-    # print(sample_text_embedding)
-    # print(sample_text)
-    # print(len(sample_text_embedding))
+    # 3. Load LLM for generation
+    llm = RAGLLM(rag_cfg['llm_type'], rag_cfg['llm_name']).load_model(**rag_cfg) # TODO - pass args
 
-    # Choose and set LLM for generation
-    # Llama-index supports OpenAI, Cohere, AI21 and HuggingFace LLMs
-    # https://docs.llamaindex.ai/en/stable/module_guides/models/llms/usage_custom.html
-    # Using local HuggingFace LLM - Llama-2-7b
-    print(f'Loading {llm_type} LLM model ...')
-    if llm_type == 'local':
-        llm = HuggingFaceLLM(
-            tokenizer_name="/model-weights/Llama-2-7b-chat-hf",
-            model_name="/model-weights/Llama-2-7b-chat-hf",
-            device_map="auto",
-            context_window=4096,
-            max_new_tokens=256,
-            generate_kwargs={"temperature": 1.0, "top_p": 1.0, "do_sample": False}, # greedy decoding
-            # model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": True},
-        )
-    elif llm_type == 'openai':
-        # TODO - Add open ai llm
-        # llm = OpenAI()
-        raise NotImplementedError
-
-    # Use service context to set the LLM, embedding model, etc.
-    # Set it globally to avoid passing it to every class
+    # Use service context to set the node parser, embedding model, LLM, etc.
     # TODO - Add pompt_helper (if required)
     service_context = ServiceContext.from_defaults(
-        embed_model=embed_model,
         node_parser=node_parser,
+        embed_model=embed_model,
         llm=llm,
     )
+    # Set it globally to avoid passing it to every class, this sets it even for rag_utils.py
     set_global_service_context(service_context)
 
-    # 2. Create/load index using the appropriate vector store
-    # Use storage context to set custom vector store
-    # Available options: https://docs.llamaindex.ai/en/stable/module_guides/storing/vector_stores.html
-    # Use Chroma: https://docs.llamaindex.ai/en/stable/examples/vector_stores/ChromaIndexDemo.html
-    # LangChain vector stores: https://python.langchain.com/docs/modules/data_connection/vectorstores/
-    chroma_client = chromadb.Client()
-    chroma_collection = chroma_client.create_collection(name="pubmed_qa")
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    index = VectorStoreIndex.from_documents(docs, storage_context=storage_context)
-    
-    # # Persist index to disk and reload
-    # persist_dir="./index_store/"
-    # index.storage_context.persist(persist_dir=persist_dir)
-    # storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
-    # index = load_index_from_storage(storage_context)
+
+    ### STAGE 3 - Create/load index using the appropriate vector store
+    index = RAGIndex(db_type=rag_cfg['vector_db_type'], db_name=rag_cfg['vector_db_name']).create_index(docs)
 
 
-    ### QUERYING STAGE
+    ### STAGE 4 - Build query engine
     # Now build a query engine using retriever, response_synthesizer and node_postprocessor (add this later)
-    # https://docs.llamaindex.ai/en/stable/understanding/querying/querying.html
-    # TODO - Check other args for RetrieverQueryEngine
+    query_engine = RAGQueryEngine(
+        retriever_type=rag_cfg['retriever_type'], vector_index=index, llm_model_name=rag_cfg['llm_name']).create(
+            similarity_top_k=rag_cfg['retriever_similarity_top_k'], response_mode=rag_cfg['response_mode'])
 
-    # Other retrievers can be used based on the type of index: List, Tree, Knowledge Graph, etc.
-    # https://docs.llamaindex.ai/en/stable/api_reference/query/retrievers.html
-    # Find LlamaIndex equivalents for the following:
-    # Check MultiQueryRetriever from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/MultiQueryRetriever
-    # Check Contextual compression from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/contextual_compression/
-    # Check Ensemble Retriever from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/ensemble
-    # Check self-query from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/self_query
-    # Check WebSearchRetriever from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/web_research
-    retriever = VectorIndexRetriever(
-        index=index,
-        similarity_top_k=3,
-        )
 
-    # # Node postprocessor: Porcessing nodes after retrieval before passing to the LLM for generation
-    # # Re-ranking step can be performed here!
-    # # Nodes can be re-ordered to include more relevant ones at the top: https://python.langchain.com/docs/modules/data_connection/document_transformers/post_retrieval/long_context_reorder
-    # # https://docs.llamaindex.ai/en/stable/module_guides/querying/node_postprocessors/node_postprocessors.html
-    # # node_postprocessor = [SimilarityPostprocessor(similarity_cutoff=0.5)]
-    # # node_postprocessor = [LLMRerank(top_n=2)]
-    # node_postprocessor = [
-    #     SentenceEmbeddingOptimizer(
-    #         embed_model=service_context.embed_model, 
-    #         percentile_cutoff=0.5
-    #         )]
+    # Finally query the model!
+    random.seed(41)
+    sample_idx = random.randint(0, len(pubmed_data)-1)
+    sample_elm = pubmed_data[sample_idx]
+    # print(sample_elm)
 
-    # Other response modes: https://docs.llamaindex.ai/en/stable/module_guides/querying/response_synthesizers/root.html#configuring-the-response-mode
-    qa_prompt_tmpl = (
-    "Context information is below.\n"
-    "---------------------\n"
-    "{context_str}\n"
-    "---------------------\n"
-    "Given the context information and not prior knowledge, answer the query. "
-    "If your answer is in favour of the query, end your response with 'yes' otherwise end your response with 'no'.\n"
-    "Query: {query_str}\n"
-    "Answer: "
-    )
-    llama2_chat_tmpl = ("<s>[INST] <<SYS>>\n{sys_msg}\n<</SYS>>\n\n{qa_prompt} [/INST]")
-    llama2_chat_sys_msg = (
-        "You are a helpful and honest assistant. "
-        "Your answer should only be based on the context information provided."
-    )
-    qa_prompt_tmpl = llama2_chat_tmpl.format_map({'sys_msg': llama2_chat_sys_msg, 'qa_prompt': qa_prompt_tmpl})
-    qa_prompt_tmpl = PromptTemplate(qa_prompt_tmpl)
-    response_synthesizer = get_response_synthesizer(response_mode='compact', text_qa_template=qa_prompt_tmpl) # compact
-
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        # node_postprocessors=node_postprocessor
-        response_synthesizer=response_synthesizer,
-    )
-
-    # # Finally query the model!
-    # random.seed(41)
-    # sample_idx = random.randint(0, len(pubmed_data)-1)
-    # sample_elm = pubmed_data[sample_idx]
-    # # print(sample_elm)
-
-    # query = sample_elm['question']
-    # print(f'QUERY: {query}\n')
+    query = sample_elm['question']
+    response = query_engine.query(query)
+    print(f'QUERY: {query}')
+    print(f'RESPONSE: {response}')
+    print(f'YES/NO: {extract_yes_no(response.response)}')
+    print(f'GT ANSWER: {sample_elm["answer"]}')
+    print(f'GT LONG ANSWER: {sample_elm["long_answer"]}')
 
     # retrieved_nodes = retriever.retrieve(query)
     # for node in retrieved_nodes:
@@ -197,17 +115,7 @@ def main():
     #     print(node.score)
     #     print('\n')
 
-    # response = query_engine.query(query)
-
-    # print(f'QUERY: {query}')
-    # print(f'RESPONSE: {response}')
-    # print(f'YES/NO: {extract_yes_no(response.response)}')
-    # print(f'GT ANSWER: {sample_elm["answer"]}')
-    # print(f'GT LONG ANSWER: {sample_elm["long_answer"]}')
-
-    # pubmed_data = pubmed_data[:5]
-
-    print(f'Overall Acc: {evaluate(pubmed_data, query_engine)}')
+    # print(f'Overall Acc: {evaluate(pubmed_data, query_engine)}')
 
 
 if __name__ == "__main__":
--- pubmed_qa/rag_utils.py
+++ pubmed_qa/rag_utils.py
@@ -1,8 +1,20 @@
 import os
 import re
 import numpy as np
+import chromadb
+
 from tqdm import tqdm
-from llama_index import SimpleDirectoryReader
+from llama_index import (
+    SimpleDirectoryReader, VectorStoreIndex, PromptTemplate, 
+    load_index_from_storage, get_response_synthesizer
+)
+from llama_index.embeddings import HuggingFaceEmbedding, OpenAIEmbedding
+from llama_index.llms import HuggingFaceLLM, OpenAI
+from llama_index.vector_stores import ChromaVectorStore
+from llama_index.storage.storage_context import StorageContext
+from llama_index.retrievers import VectorIndexRetriever
+from llama_index.query_engine import RetrieverQueryEngine
+from llama_index.postprocessor import SimilarityPostprocessor, LLMRerank, SentenceEmbeddingOptimizer
 
 
 class DocumentReader():
@@ -37,6 +49,178 @@ class DocumentReader():
                     doc.excluded_embed_metadata_keys = all_metadata_keys
 
         return docs
+
+
+class RAGEmbedding():
+    '''
+    Llama-index supports embedding models from OpenAI, Cohere, LangChain, HuggingFace, etc. 
+    We can also build out custom embedding model. 
+    https://docs.llamaindex.ai/en/stable/module_guides/models/embeddings.html
+    '''
+    def __init__(self, model_type, model_name):
+        self.model_type = model_type
+        self.model_name = model_name
+
+    def load_model(self):
+        print(f'Loading {self.model_type} embedding model ...')
+        if self.model_type == 'hf':
+            # Using bge base HuggingFace embeddings, can choose others based on leaderboard: 
+            # https://huggingface.co/spaces/mteb/leaderboard
+            embed_model = HuggingFaceEmbedding(model_name=self.model_name) # max_length does not have any effect?
+
+        elif self.model_type == 'openai':
+            # TODO - Add open ai embedding model
+            # embed_model = OpenAIEmbedding()
+            raise NotImplementedError
+
+        # sample_text_embedding = embed_model.get_text_embedding(sample_text)
+        # print(sample_text_embedding)
+        # print(sample_text)
+        # print(len(sample_text_embedding))
+        
+        return embed_model
+
+
+class RAGLLM():
+    '''
+    Llama-index supports OpenAI, Cohere, AI21 and HuggingFace LLMs
+    https://docs.llamaindex.ai/en/stable/module_guides/models/llms/usage_custom.html
+    '''
+    def __init__(self, llm_type, llm_name):
+        self.llm_type = llm_type
+        self.llm_name = llm_name
+
+    def load_model(self, **kwargs):
+        print(f'Loading {self.llm_type} LLM model ...')
+        gen_arg_keys = ['temperature', 'top_p', 'top_k', 'do_sample']
+        gen_kwargs = {k: v for k, v in kwargs.items() if k in gen_arg_keys}
+        if self.llm_type == 'local':
+            # Using local HuggingFace LLM stored at /model-weights
+            llm = HuggingFaceLLM(
+                tokenizer_name=f"/model-weights/{self.llm_name}",
+                model_name=f"/model-weights/{self.llm_name}",
+                device_map="auto",
+                context_window=4096,
+                max_new_tokens=kwargs['max_new_tokens'],
+                generate_kwargs=gen_kwargs,
+                # model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": True},
+            )
+        elif self.llm_type == 'openai':
+            # TODO - Add open ai llm
+            # llm = OpenAI()
+            raise NotImplementedError
+
+        return llm
+
+
+class RAGIndex():
+    '''
+    Use storage context to set custom vector store
+    Available options: https://docs.llamaindex.ai/en/stable/module_guides/storing/vector_stores.html
+    Use Chroma: https://docs.llamaindex.ai/en/stable/examples/vector_stores/ChromaIndexDemo.html
+    LangChain vector stores: https://python.langchain.com/docs/modules/data_connection/vectorstores/
+    '''
+    def __init__(self, db_type, db_name):
+        self.db_type = db_type
+        self.db_name = db_name
+        self._persist_dir = './.index_store/'
+
+    def create_index(self, docs, save=True):
+        # Only supports ChromaDB as of now
+        if self.db_type == 'chromadb':
+            chroma_client = chromadb.Client()
+            chroma_collection = chroma_client.create_collection(name=self.db_name)
+            vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        else:
+            raise NotImplementedError(f'Incorrect vector db type - {self.db_type}')
+        
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        index = VectorStoreIndex.from_documents(docs, storage_context=storage_context)
+        if save:
+            if not os.path.isdir(self._persist_dir):
+                os.mkdir(self._persist_dir)
+            index.storage_context.persist(persist_dir=self._persist_dir)
+            # TODO - Figure out reload
+
+        return index
+
+
+class RAGQueryEngine():
+    '''
+    https://docs.llamaindex.ai/en/stable/understanding/querying/querying.html
+    TODO - Check other args for RetrieverQueryEngine
+    '''
+    def __init__(self, retriever_type, vector_index, llm_model_name):
+        self.retriever_type = retriever_type
+        self.index = vector_index
+        self.llm_model_name = llm_model_name
+        self.retriever = None
+        self.node_postprocessors = None
+        self.response_synthesizer = None
+
+    def create(self, similarity_top_k, response_mode):
+        self.set_retriever(similarity_top_k)
+        self.set_response_synthesizer(response_mode)
+        query_engine = RetrieverQueryEngine(
+            retriever=self.retriever,
+            # node_postprocessors=self.node_postprocessor
+            response_synthesizer=self.response_synthesizer,
+            )
+        return query_engine
+    
+    def set_retriever(self, similarity_top_k):
+        # Other retrievers can be used based on the type of index: List, Tree, Knowledge Graph, etc.
+        # https://docs.llamaindex.ai/en/stable/api_reference/query/retrievers.html
+        # Find LlamaIndex equivalents for the following:
+        # Check MultiQueryRetriever from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/MultiQueryRetriever
+        # Check Contextual compression from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/contextual_compression/
+        # Check Ensemble Retriever from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/ensemble
+        # Check self-query from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/self_query
+        # Check WebSearchRetriever from LangChain: https://python.langchain.com/docs/modules/data_connection/retrievers/web_research
+        if self.retriever_type == 'vector_index':
+            self.retriever = VectorIndexRetriever(
+                index=self.index,
+                similarity_top_k=similarity_top_k,
+                )
+        else:
+            raise NotImplementedError(f'Incorrect retriever type - {self.retriever_type}')
+
+    def set_node_postprocessors(self):
+        # # Node postprocessor: Porcessing nodes after retrieval before passing to the LLM for generation
+        # # Re-ranking step can be performed here!
+        # # Nodes can be re-ordered to include more relevant ones at the top: https://python.langchain.com/docs/modules/data_connection/document_transformers/post_retrieval/long_context_reorder
+        # # https://docs.llamaindex.ai/en/stable/module_guides/querying/node_postprocessors/node_postprocessors.html
+        # # node_postprocessor = [SimilarityPostprocessor(similarity_cutoff=0.5)]
+        # # node_postprocessor = [LLMRerank(top_n=2)]
+        # node_postprocessor = [
+        #     SentenceEmbeddingOptimizer(
+        #         embed_model=service_context.embed_model, 
+        #         percentile_cutoff=0.5
+        #         )]
+        raise NotImplementedError
+
+    def set_response_synthesizer(self, response_mode):
+        # Other response modes: https://docs.llamaindex.ai/en/stable/module_guides/querying/response_synthesizers/root.html#configuring-the-response-mode
+        qa_prompt_tmpl = (
+            "Context information is below.\n"
+            "---------------------\n"
+            "{context_str}\n"
+            "---------------------\n"
+            "Given the context information and not prior knowledge, answer the query. "
+            "If your answer is in favour of the query, end your response with 'yes' otherwise end your response with 'no'.\n"
+            "Query: {query_str}\n"
+            "Answer: "
+            )
+        if self.llm_model_name == 'Llama-2-7b-chat-hf':
+            llama2_chat_tmpl = ("<s>[INST] <<SYS>>\n{sys_msg}\n<</SYS>>\n\n{qa_prompt} [/INST]")
+            llama2_chat_sys_msg = (
+                "You are a helpful and honest assistant. "
+                "Your answer should only be based on the context information provided."
+            )
+            qa_prompt_tmpl = llama2_chat_tmpl.format_map({'sys_msg': llama2_chat_sys_msg, 'qa_prompt': qa_prompt_tmpl})
+        qa_prompt_tmpl = PromptTemplate(qa_prompt_tmpl)
+
+        self.response_synthesizer = get_response_synthesizer(response_mode=response_mode, text_qa_template=qa_prompt_tmpl)
 
 
 def extract_yes_no(resp):
