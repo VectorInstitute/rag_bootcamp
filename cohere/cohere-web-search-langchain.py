0c4ffff252e7fbba38d06a4e629bad4cf99692cb
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2024-01-02 15:45:53 -0500

    Fixed Cohere web search so it now returns good results

--- cohere/cohere-web-search-langchain.py
+++ cohere/cohere-web-search-langchain.py
@@ -9,15 +9,13 @@ import os
 from pathlib import Path
 import requests
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import TextLoader
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatCohere
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.llms import Cohere
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import CohereRerank
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
 
 
 def pretty_print_docs(docs):
@@ -34,18 +32,13 @@ def main():
     os.environ["COHERE_API_KEY"] = open(Path.home() / ".cohere.key", "r").read().strip()
 
     # Start with making a generation request without RAG augmentation
-    query = "Who won the last World Series of baseball, and in what year?"
-    llm = ChatCohere()
-    print(f"Sending non-RAG augmented generation request for query: {query}")
-    message = [
-        HumanMessage(
-            content=query
-        )
-    ]
-    result = llm(message)
-    print(f"Result: {result}")
+    query = "Who won the 2023 World Series of baseball?"
+    llm = Cohere()
+    print(f"*** Sending non-RAG augmented generation request for query: {query}\n")
+    result = llm(query)
+    print(f"Result: {result}\n")
 
-    print(f"Now retrieve results from a Google web search for the same query")
+    print(f"*** Now retrieve results from a Google web search for the same query\n")
     result_text = ""
     for result_url in search(query, tld="com", num=10, stop=10, pause=2):
         response = requests.get(result_url)
@@ -53,16 +46,15 @@ def main():
         result_text = result_text + soup.get_text()
 
     # Split the result text into smaller chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     texts = text_splitter.split_text(result_text)
-    print(f"*** Number of text chunks: {len(texts)}")
-    #print(f"Contents of first text chunk: {texts[0]}")
+    print(f"*** Number of text chunks: {len(texts)}\n")
 
     # Define Embeddings Model
     model_name = "BAAI/bge-small-en-v1.5"
     encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
 
-    print(f"*** Setting up the embeddings model...")
+    print(f"*** Setting up the embeddings model...\n")
     embeddings = HuggingFaceBgeEmbeddings(
         model_name=model_name,
         model_kwargs={'device': 'cuda'},
@@ -70,7 +62,7 @@ def main():
     )
 
     # Set up the base vector store retriever
-    print(f"*** Setting up the base vector store retriever")
+    print(f"*** Setting up the base vector store retriever\n")
     vectorstore = FAISS.from_texts(texts, embeddings)
     retriever = vectorstore.as_retriever(search_kwargs={"k": 20})
 
@@ -79,7 +71,7 @@ def main():
     #pretty_print_docs(docs)
 
     # Applying Reranking with CohereRerank
-    print(f"*** Applying re-ranking with CohereRerank")
+    print(f"*** Applying reranking with CohereRerank\n")
     compressor = CohereRerank()
     compression_retriever = ContextualCompressionRetriever(
         base_compressor=compressor, base_retriever=retriever
@@ -88,11 +80,12 @@ def main():
     pretty_print_docs(compressed_docs)
 
     # The Generation part of RAG Pipeline
-    print(f"*** Now do the RAG generation with query: {query}")
+    print(f"*** Now do the reranked RAG generation with query: {query}\n")
     qa = RetrievalQA.from_chain_type(llm=llm,
             chain_type="stuff",
-            retriever=retriever)
-    print(f"*** Running generation: {qa.run(query=query)}")
+            retriever=compression_retriever)
+    print(f"*** Running generation...\n")
+    print(f"Result: {qa.run(query=query)}")
 
 
 if __name__ == "__main__":
