8437b542b4539a98ad78f8dd353099ebd92f85b0
Omkar Dige <oyedige@gmail.com>
2024-01-18 20:53:33 -0500

    [1] Added support for jsonl [2] Write each sample as a separate document

--- pubmed_qa/task_dataset.py
+++ pubmed_qa/task_dataset.py
@@ -36,27 +36,36 @@ class PubMedQATaskDataset(data.Dataset):
     def __len__(self):
         return len(self.data)
 
-    def mock_knowledge_base(self, output_dir, samples_per_file=500, sep='\n', jsonl=False):
+    def mock_knowledge_base(self, output_dir, one_file_per_sample=False, samples_per_file=500, sep='\n', jsonl=False):
         """
         Write PubMed contexts to a text file, newline seperated
         """
         pubmed_kb_dir = os.path.join(output_dir, 'pubmed_doc')
-        os.makedirs(pubmed_kb_dir, exist_ok=False)
-        
-        context_str = ''
-        context_files = []
-        for idx in range(len(self.data)):
-            if (idx + 1) % samples_per_file == 0:
-                context_files.append(context_str.rstrip(sep))
-            else:
-                if jsonl:
-                    context_elm_str = json.dumps({'id': self.data[idx]["id"],'context': self.data[idx]["context"]})
-                else:
-                    context_elm_str = self.data[idx]["context"]
-                context_str += f'{context_elm_str}{sep}'
+        os.makedirs(pubmed_kb_dir, exist_ok=True)
 
         file_ext = 'jsonl' if jsonl else 'txt'
-        for file_idx in range(len(context_files)):
-            filepath = os.path.join(pubmed_kb_dir, f'context{file_idx}.{file_ext}')
-            with open(filepath, 'w') as f:
-                f.write(context_files[file_idx])
+
+        if not one_file_per_sample:
+            context_str = ''
+            context_files = []
+            for idx in range(len(self.data)):
+                if (idx + 1) % samples_per_file == 0:
+                    context_files.append(context_str.rstrip(sep))
+                else:
+                    if jsonl:
+                        context_elm_str = json.dumps({'id': self.data[idx]["id"],'context': self.data[idx]["context"]})
+                    else:
+                        context_elm_str = self.data[idx]["context"]
+                    context_str += f'{context_elm_str}{sep}'
+
+            for file_idx in range(len(context_files)):
+                filepath = os.path.join(pubmed_kb_dir, f'context{file_idx}.{file_ext}')
+                with open(filepath, 'w') as f:
+                    f.write(context_files[file_idx])
+        
+        else:
+            assert not jsonl, "Does not support jsonl if one_file_per_sample is True"
+            for idx in range(len(self.data)):
+                filepath = os.path.join(pubmed_kb_dir, f'{self.data[idx]["id"]}.{file_ext}')
+                with open(filepath, 'w') as f:
+                    f.write(self.data[idx]["context"])
